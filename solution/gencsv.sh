d13af860137ef7b5f6ba6dc64c842f8c766dec6c
Jainsi1 <75137939+Jainsi1@users.noreply.github.com>
2024-05-21 01:21:55 +0530

    Update gencsv.sh

--- solution/gencsv.sh
+++ solution/gencsv.sh
@@ -17,10 +17,14 @@ filename="inputdata"
 
 echo "Generating $filename..."
 
+# Remove inputFile if it exists
+if [ -e "inputdata" ]; then
+    rm inputdata
+fi
+
 for ((i=start_index; i<=end_index; i++)); do
     rand_num=$((1 + $RANDOM ))
     echo "$i,$rand_num" >> "$filename"
 done
 
 echo "File $filename generated successfully."
-
